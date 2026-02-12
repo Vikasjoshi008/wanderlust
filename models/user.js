cc5ca6db39ff0233399a1127e776e90feae4673d
Vikas Joshi <vikasjoshi10889@gmail.com>
2026-02-12 13:41:48 +0530

    fix user model

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 const schema=mongoose.Schema;
 const passportlocalmongoose=require("passport-local-mongoose");
 
-const userschema= new schema ({
+const userschema = new schema ({
     email: {
         type: String,
         required: true,
